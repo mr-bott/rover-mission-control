e31f22ab5a7a13da48b348d19e7916b6c9963b9a
Murali Krishna <muralikrishna8309@gmail.com>
2026-03-28 16:19:56 +0530

    added extra comments for better understanding

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
-#Ordered directions for rotation logic
+# Ordered directions for rotation logic
 DIRECTIONS = ['N', 'E', 'S', 'W']
 
-#Movement vectors for each direction:
-#change in x and y when moving forward
+# Movement vectors for each direction:
+# Change in x and y when moving forward
 MOVE_MAP = {
     'N': (0, 1),   # Move up
     'E': (1, 0),   # Move right
